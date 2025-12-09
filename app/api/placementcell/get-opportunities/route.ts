1e0b1007692f1a6ef7d1b8c48be82c16badee2be
xcurx <suj5al78@gmail.com>
2025-12-09 13:00:38 +0530

    feat: Add application status filter and CSV export functionality to OpportunityDetailPage

--- app/api/placementcell/get-opportunities/route.ts
+++ app/api/placementcell/get-opportunities/route.ts
@@ -2,7 +2,7 @@ import { auth } from "@/auth";
 import { PrismaClient } from "@/lib/generated/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-const prisma = new PrismaClient
+const prisma = new PrismaClient()
 
 export const GET = async (req: NextRequest) => {
     const session = await auth();
@@ -30,6 +30,7 @@ export const GET = async (req: NextRequest) => {
     
         return NextResponse.json({ opportunities }, { status: 200 });
     } catch (error) {
+        console.error("Error fetching opportunities:", error);
         return NextResponse.json({ message: "Internal Server Error", error }, { status: 500 });
     }
 }
